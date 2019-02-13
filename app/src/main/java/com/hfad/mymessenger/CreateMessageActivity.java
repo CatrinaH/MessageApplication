63bf0190d78b9293cc16529d2187369e7ef77ef0
CatrinaH <caricokid@gmail.com>
2019-02-12 17:11:15 -0700

    Replace initial code in create message activity java.

--- app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -1,13 +1,14 @@
 package com.hfad.mymessenger;
-
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-
-public class CreateMessageActivity extends AppCompatActivity {
-
+import android.view.View;
+public class CreateMessageActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+    }
+    //Call onSendMessage() when the button is clicked
+    public void onSendMessage(View view) {
     }
 }
